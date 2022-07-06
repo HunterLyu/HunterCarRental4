7f9393e1f563325d9f704fbcae5e859631c725e9
Shiyu Lyu <hunterlucas@163.com>
2022-07-06 21:54:59 +0800

    update to run in tomcat

--- src/main/java/org/hunter/carrental4/HunterCarRental4Application.java
+++ src/main/java/org/hunter/carrental4/HunterCarRental4Application.java
@@ -2,12 +2,18 @@ package org.hunter.carrental4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HunterCarRental4Application {
+public class HunterCarRental4Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HunterCarRental4Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HunterCarRental4Application.class);
+    }
 }
